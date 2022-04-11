08c1fc136fe161500cda651a266bd7e4dfd0f374
Hassan Farooqui <hassanfarooqui817@gmail.com>
2022-04-11 12:28:03 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {UserService} from './user.service'
+import { User } from './User';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-frontend';
+
+  
+  users: User[] =[];
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data: User[]) => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './User';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Access-Control-Allow-Origin" : "",
+    "Allow": "GET",
+    "Content-type": "Application/json"
+  })
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = "http://localhost:8080/api/employees";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${this.baseUrl}`,httpOptions);
+  }
+}
