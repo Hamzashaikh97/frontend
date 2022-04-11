b041e8d72ae19455edfd396102ec58a916cad43d
Hassan Farooqui <hassanfarooqui817@gmail.com>
2022-04-11 15:10:50 +0500

    pdf download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild  } from '@angular/core';
 import {UserService} from './user.service'
 import { User } from './User';
+declare var require: any;
+
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+const htmlToPdfmake = require("html-to-pdfmake");
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
@@ -20,4 +26,15 @@ export class AppComponent {
       this.users = data;
     });
   }
+  @ViewChild('pdfTable')
+  pdfTable!: ElementRef;
+  
+  public downloadAsPDF() {
+    const pdfTable = this.pdfTable.nativeElement;
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).download(); 
+     
+  }
+
 }
